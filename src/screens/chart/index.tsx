e12f926e0b691d5205011c3fcad7fab15b0817b7
neosarchizo <neosarchizo@gmail.com>
2022-03-22 11:26:14 +0900

    feat: update chart

--- src/screens/chart/index.tsx
+++ src/screens/chart/index.tsx
@@ -1,15 +1,110 @@
-import {VFC} from 'react'
+import {VFC, useState, useCallback, useMemo, useEffect} from 'react'
+import {Grid} from '@material-ui/core'
 
-import Layout from '../../components/layout'
+import {Layout, ModelSelect, RefreshIntervalSelect, SerialNumberSelect} from '../../components'
 import {useI18n} from '../../utils/i18n'
-import {Container, Body} from './styles'
+import {Container, GridContainer, Body} from './styles'
+import {DeviceModel, EventListener, ResGetData} from '../../contexts/device/types'
+import {RefreshIntervalType} from '../../types'
+import {useDevice} from '../../contexts/device'
+import {useIntervalOnlyEffect} from '../../utils/use-interval'
 
 const Main: VFC = () => {
   const {t} = useI18n()
 
+  const [modelOption, setModelOption] = useState<DeviceModel>('PM2008')
+  const [serialNumberOption, setSerialNumberOption] = useState<string>('NONE')
+  const [refreshIntervalOption, setRefreshIntervalOption] = useState<RefreshIntervalType>('5_SECS')
+
+  const [, deviceManager] = useDevice()
+  const [, setData] = useState<any[]>([])
+
+  const refreshInterval = useMemo<number | null>(() => {
+    let result: number | null = null
+
+    switch (refreshIntervalOption) {
+      case '5_SECS': {
+        result = 5000
+        break
+      }
+      case '10_SECS': {
+        result = 10000
+        break
+      }
+      case '30_SECS': {
+        result = 30000
+        break
+      }
+      case '1_MIN': {
+        result = 60000
+        break
+      }
+      default:
+        break
+    }
+
+    return result
+  }, [refreshIntervalOption])
+
+  const handleOnIntervalTimeout = useCallback<() => void>(() => {
+    if (serialNumberOption === 'NONE') {
+      return
+    }
+    deviceManager.getData(modelOption, serialNumberOption)
+  }, [modelOption, serialNumberOption, deviceManager])
+
+  useIntervalOnlyEffect(handleOnIntervalTimeout, refreshInterval)
+
+  const handleOnGetData = useCallback<EventListener>(
+    (event) => {
+      const {type, payload} = event
+
+      if (type !== 'GET_DATA') {
+        return
+      }
+
+      const param = payload as ResGetData
+
+      const {model: m, data: d} = param
+
+      if (m !== modelOption) {
+        return
+      }
+
+      setData(d)
+    },
+    [modelOption],
+  )
+
+  useEffect(() => {
+    const sub = deviceManager.subscribe(handleOnGetData)
+
+    return () => {
+      sub.unsubscribe()
+    }
+  }, [deviceManager, handleOnGetData])
+
   return (
     <Layout title={t('chart')}>
       <Container>
+        <GridContainer>
+          <Grid item xs={12} sm={6} md={3}>
+            <ModelSelect value={modelOption} onChange={setModelOption} />
+          </Grid>
+          <Grid item xs={12} sm={6} md={3}>
+            <SerialNumberSelect
+              model={modelOption}
+              value={serialNumberOption}
+              onChange={setSerialNumberOption}
+            />
+          </Grid>
+          <Grid item xs={12} sm={6} md={3}>
+            <RefreshIntervalSelect
+              value={refreshIntervalOption}
+              onChange={setRefreshIntervalOption}
+            />
+          </Grid>
+        </GridContainer>
         <Body />
       </Container>
     </Layout>
--- src/screens/chart/styles/index.ts
+++ src/screens/chart/styles/index.ts
@@ -1,12 +1,16 @@
 import styled from 'styled-components'
-import {DataGrid} from '@mui/x-data-grid'
-import {IconButton} from '@material-ui/core'
+import {Grid} from '@material-ui/core'
 
 export const Container = styled.div`
   height: 100%;
   width: 100%;
   display: flex;
   flex-direction: column;
+`
+
+export const GridContainer = styled(Grid).attrs({container: true, spacing: 3})`
+  width: 100%;
+  padding: 10px;
 `
 
 export const TableContainer = styled.div`
@@ -16,8 +20,10 @@ export const TableContainer = styled.div`
 
 export const Body = styled.div`
   flex: 1;
+  width: 100%;
   display: flex;
   flex-direction: column;
+  background: green;
 `
 
 export const DataGridContainer = styled.div`
@@ -35,20 +41,7 @@ export const RowButton = styled.div`
   padding-right: 10px;
 `
 
-export const MyDataGrid = styled(DataGrid).attrs({
-  components: {
-    NoRowsOverlay: () => {
-      return null
-    },
-  },
-  checkboxSelection: false,
-})``
-
 export const Row = styled.div`
   display: flex;
   flex-direction: row;
 `
-
-export const MyIconButton = styled(IconButton).attrs({
-  component: 'span',
-})``
