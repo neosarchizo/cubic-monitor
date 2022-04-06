1d80b9d9e80f99565217827b2ff41c1aedc7a412
neosarchizo <neosarchizo@gmail.com>
2022-04-06 13:08:55 +0900

    feat: add QUERY_GET_RANGE

--- electron/utils/device-manager/queries/index.ts
+++ electron/utils/device-manager/queries/index.ts
@@ -5,3 +5,9 @@ export const QUERY_GET_DATA: (name: string, serialNumber: string) => string = (
   name,
   serialNumber,
 ) => `SELECT * FROM ${name} WHERE SERIAL_NUMBER="${serialNumber}" ORDER BY ID DESC LIMIT 1000;`
+
+export const QUERY_GET_RANGE: (name: string, serialNumber: string) => string = (
+  name,
+  serialNumber,
+) =>
+  `SELECT MAX(TIMESTAMP) AS max, MIN(TIMESTAMP) AS min  FROM ${name} WHERE SERIAL_NUMBER == "${serialNumber}";`
