5ba4124e30eca721ec5a36aecc76aefc58e82db1
neosarchizo <neosarchizo@gmail.com>
2022-05-18 10:48:27 +0900

    feat: add cb-hcho-v4

---
+++ electron/utils/device-manager/models/cb-hcho-v4/constants/index.ts
@@ -0,0 +1,15 @@
+export const CMD_READ = 0x1
+export const CMD_AUTO_CALIBRATION = 0x3
+
+export const SENSOR_STATUS_NORMAL_WORKING = 0
+export const SENSOR_STATUS_ANORMALY = 1
+export const SENSOR_STATUS_RECOVERY_PROCESS = 2
+export const SENSOR_STATUS_HIGH_FORMALDEHYDE = 3
+export const SENSOR_STATUS_HIGH_ALCOHOL = 4
+
+export const AUTO_CALIBRATION_OFF = 0
+export const AUTO_CALIBRATION_ON = 1
+
+export const ACS_MANUAL = 0
+export const ACS_AUTO = 1
+export const ACS_ZERO_CLEARING = 2
