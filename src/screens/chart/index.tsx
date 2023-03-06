e67b6d193385333b5b76e668bd7077c41636c446
neosarchizo <neosarchizo@gmail.com>
2023-03-06 16:01:54 +0900

    feat: add legendsState

--- src/screens/chart/index.tsx
+++ src/screens/chart/index.tsx
@@ -5,7 +5,16 @@ import {Layout, DataLoader} from '../../components'
 import {useI18n} from '../../utils/i18n'
 import {Container, Body, MyPlot, LegendPanel} from './styles'
 import {ResGetData} from '../../contexts/device/types'
-import {Trace, Layout as LayoutType} from './types'
+import {
+  Trace,
+  Layout as LayoutType,
+  PM2008LegendState,
+  CM1106LegendState,
+  CM1107LegendState,
+  AM1008WKLegendState,
+  CBHCHOV4LegendState,
+  AM1002LegendState,
+} from './types'
 import {CM1106Data} from '../../contexts/device/models/cm1106/types'
 import {CM1107Data} from '../../contexts/device/models/cm1107/types'
 import {PM2008Data} from '../../contexts/device/models/pm2008/types'
@@ -26,9 +35,70 @@ const Main: VFC = () => {
     title: '',
     datarevision: '',
   })
+  const [legendsState] = useState<{
+    pm2008: PM2008LegendState
+    cm1106: CM1106LegendState
+    cm1107: CM1107LegendState
+    am1008wk: AM1008WKLegendState
+    cbhchov4: CBHCHOV4LegendState
+    am1002: AM1002LegendState
+  }>({
+    pm2008: {
+      pm1P0Grimm: false,
+      pm2P5Grimm: false,
+      pm10PGrimm: false,
+      pm1P0Tsi: false,
+      pm2P5Tsi: false,
+      pm10PTsi: false,
+      particleNumber0p3UmAbove: false,
+      particleNumber0p5UmAbove: false,
+      particleNumber1p0UmAbove: false,
+      particleNumber2p5UmAbove: false,
+      particleNumber5p0UmAbove: false,
+      particleNumber10pUmAbove: false,
+    },
+    cm1106: {
+      co2: false,
+    },
+    cm1107: {
+      co2: false,
+    },
+    am1008wk: {
+      co2: false,
+      voc: false,
+      relatedHumidity: false,
+      temperature: false,
+      pm1P0Grimm: false,
+      pm2P5Grimm: false,
+      pm10PGrimm: false,
+      vocNowRef: false,
+      vocRefRValue: false,
+      vocNowRValue: false,
+      pmSensorState: false,
+    },
+    cbhchov4: {
+      hcho: false,
+      voc: false,
+      temperature: false,
+      humidity: false,
+      tvoc: false,
+      sensorStatus: false,
+      autoCalibrationSwitch: false,
+    },
+    am1002: {
+      tvoc: false,
+      pm1p0Grimm: false,
+      pm2p5Grimm: false,
+      pm10pGrimm: false,
+      temperature: false,
+      humidity: false,
+    },
+  })
 
   const traces = useMemo<Trace[]>(() => {
     const newTraces: Trace[] = []
+
+    const {pm2008, cm1106, cm1107, am1008wk, cbhchov4, am1002} = legendsState
 
     if (snChart === 'NONE') {
       setLayout({
@@ -98,29 +168,74 @@ const Main: VFC = () => {
           arrParticleNumber10pUmAbove.push(particleNumber10pUmAbove)
         })
 
-        newTraces.push(getTrace(arrCreatedAt, arrPm1p0Grimm, t('pm1P0Grimm')))
-        newTraces.push(getTrace(arrCreatedAt, arrPm2p5Grimm, t('pm2P5Grimm')))
-        newTraces.push(getTrace(arrCreatedAt, arrPm10pGrimm, t('pm10PGrimm')))
-        newTraces.push(getTrace(arrCreatedAt, arrPm1p0Tsi, t('pm1P0Tsi')))
-        newTraces.push(getTrace(arrCreatedAt, arrPm2p5Tsi, t('pm2P5Tsi')))
-        newTraces.push(getTrace(arrCreatedAt, arrPm10pTsi, t('pm10PTsi')))
+        const {
+          pm1P0Grimm,
+          pm2P5Grimm,
+          pm10PGrimm,
+          pm1P0Tsi,
+          pm2P5Tsi,
+          pm10PTsi,
+          particleNumber0p3UmAbove,
+          particleNumber0p5UmAbove,
+          particleNumber1p0UmAbove,
+          particleNumber2p5UmAbove,
+          particleNumber5p0UmAbove,
+          particleNumber10pUmAbove,
+        } = pm2008
+
+        newTraces.push(getTrace(arrCreatedAt, arrPm1p0Grimm, t('pm1P0Grimm'), pm1P0Grimm))
+        newTraces.push(getTrace(arrCreatedAt, arrPm2p5Grimm, t('pm2P5Grimm'), pm2P5Grimm))
+        newTraces.push(getTrace(arrCreatedAt, arrPm10pGrimm, t('pm10PGrimm'), pm10PGrimm))
+        newTraces.push(getTrace(arrCreatedAt, arrPm1p0Tsi, t('pm1P0Tsi'), pm1P0Tsi))
+        newTraces.push(getTrace(arrCreatedAt, arrPm2p5Tsi, t('pm2P5Tsi'), pm2P5Tsi))
+        newTraces.push(getTrace(arrCreatedAt, arrPm10pTsi, t('pm10PTsi'), pm10PTsi))
         newTraces.push(
-          getTrace(arrCreatedAt, arrParticleNumber0p3UmAbove, t('particleNumber0p3UmAbove')),
+          getTrace(
+            arrCreatedAt,
+            arrParticleNumber0p3UmAbove,
+            t('particleNumber0p3UmAbove'),
+            particleNumber0p3UmAbove,
+          ),
         )
         newTraces.push(
-          getTrace(arrCreatedAt, arrParticleNumber0p5UmAbove, t('particleNumber0p5UmAbove')),
+          getTrace(
+            arrCreatedAt,
+            arrParticleNumber0p5UmAbove,
+            t('particleNumber0p5UmAbove'),
+            particleNumber0p5UmAbove,
+          ),
         )
         newTraces.push(
-          getTrace(arrCreatedAt, arrParticleNumber1p0UmAbove, t('particleNumber1p0UmAbove')),
+          getTrace(
+            arrCreatedAt,
+            arrParticleNumber1p0UmAbove,
+            t('particleNumber1p0UmAbove'),
+            particleNumber1p0UmAbove,
+          ),
         )
         newTraces.push(
-          getTrace(arrCreatedAt, arrParticleNumber2p5UmAbove, t('particleNumber2p5UmAbove')),
+          getTrace(
+            arrCreatedAt,
+            arrParticleNumber2p5UmAbove,
+            t('particleNumber2p5UmAbove'),
+            particleNumber2p5UmAbove,
+          ),
         )
         newTraces.push(
-          getTrace(arrCreatedAt, arrParticleNumber5p0UmAbove, t('particleNumber5p0UmAbove')),
+          getTrace(
+            arrCreatedAt,
+            arrParticleNumber5p0UmAbove,
+            t('particleNumber5p0UmAbove'),
+            particleNumber5p0UmAbove,
+          ),
         )
         newTraces.push(
-          getTrace(arrCreatedAt, arrParticleNumber10pUmAbove, t('particleNumber10pUmAbove')),
+          getTrace(
+            arrCreatedAt,
+            arrParticleNumber10pUmAbove,
+            t('particleNumber10pUmAbove'),
+            particleNumber10pUmAbove,
+          ),
         )
 
         setLayout({
@@ -131,7 +246,6 @@ const Main: VFC = () => {
       }
       case 'CM1106': {
         // id, createdAt, co2
-
         const cm1106Data = data as CM1106Data[]
 
         if (cm1106Data.length === 0) {
@@ -152,7 +266,9 @@ const Main: VFC = () => {
           arrCo2.push(co2)
         })
 
-        newTraces.push(getTrace(arrCreatedAt, arrCo2, t('co2')))
+        const {co2} = cm1106
+
+        newTraces.push(getTrace(arrCreatedAt, arrCo2, t('co2'), co2))
 
         setLayout({
           title: t('cm1106'),
@@ -183,7 +299,9 @@ const Main: VFC = () => {
           arrCo2.push(co2)
         })
 
-        newTraces.push(getTrace(arrCreatedAt, arrCo2, t('co2')))
+        const {co2} = cm1107
+
+        newTraces.push(getTrace(arrCreatedAt, arrCo2, t('co2'), co2))
 
         setLayout({
           title: t('cm1107'),
@@ -246,17 +364,33 @@ const Main: VFC = () => {
           arrPmSensorState.push(pmSensorState)
         })
 
-        newTraces.push(getTrace(arrCreatedAt, arrCo2, t('co2')))
-        newTraces.push(getTrace(arrCreatedAt, arrVoc, t('voc')))
-        newTraces.push(getTrace(arrCreatedAt, arrRelatedHumidity, t('relatedHumidity')))
-        newTraces.push(getTrace(arrCreatedAt, arrTemperature, t('temperature')))
-        newTraces.push(getTrace(arrCreatedAt, arrPm1P0Grimm, t('pm1P0Grimm')))
-        newTraces.push(getTrace(arrCreatedAt, arrPm2P5Grimm, t('pm2P5Grimm')))
-        newTraces.push(getTrace(arrCreatedAt, arrPm10PGrimm, t('pm10PGrimm')))
-        newTraces.push(getTrace(arrCreatedAt, arrVocNowRef, t('vocNowRef')))
-        newTraces.push(getTrace(arrCreatedAt, arrVocRefRValue, t('vocRefRValue')))
-        newTraces.push(getTrace(arrCreatedAt, arrVocNowRValue, t('vocNowRValue')))
-        newTraces.push(getTrace(arrCreatedAt, arrPmSensorState, t('pmSensorState')))
+        const {
+          co2,
+          voc,
+          relatedHumidity,
+          temperature,
+          pm1P0Grimm,
+          pm2P5Grimm,
+          pm10PGrimm,
+          vocNowRef,
+          vocRefRValue,
+          vocNowRValue,
+          pmSensorState,
+        } = am1008wk
+
+        newTraces.push(getTrace(arrCreatedAt, arrCo2, t('co2'), co2))
+        newTraces.push(getTrace(arrCreatedAt, arrVoc, t('voc'), voc))
+        newTraces.push(
+          getTrace(arrCreatedAt, arrRelatedHumidity, t('relatedHumidity'), relatedHumidity),
+        )
+        newTraces.push(getTrace(arrCreatedAt, arrTemperature, t('temperature'), temperature))
+        newTraces.push(getTrace(arrCreatedAt, arrPm1P0Grimm, t('pm1P0Grimm'), pm1P0Grimm))
+        newTraces.push(getTrace(arrCreatedAt, arrPm2P5Grimm, t('pm2P5Grimm'), pm2P5Grimm))
+        newTraces.push(getTrace(arrCreatedAt, arrPm10PGrimm, t('pm10PGrimm'), pm10PGrimm))
+        newTraces.push(getTrace(arrCreatedAt, arrVocNowRef, t('vocNowRef'), vocNowRef))
+        newTraces.push(getTrace(arrCreatedAt, arrVocRefRValue, t('vocRefRValue'), vocRefRValue))
+        newTraces.push(getTrace(arrCreatedAt, arrVocNowRValue, t('vocNowRValue'), vocNowRValue))
+        newTraces.push(getTrace(arrCreatedAt, arrPmSensorState, t('pmSensorState'), pmSensorState))
 
         setLayout({
           title: t('am1008wk'),
@@ -306,13 +440,23 @@ const Main: VFC = () => {
           arrAutoCalibrationSwitch.push(autoCalibrationSwitch)
         })
 
-        newTraces.push(getTrace(arrCreatedAt, arrHcho, t('hcho')))
-        newTraces.push(getTrace(arrCreatedAt, arrVoc, t('voc')))
-        newTraces.push(getTrace(arrCreatedAt, arrTemperature, t('temperature')))
-        newTraces.push(getTrace(arrCreatedAt, arrHumidity, t('humidity')))
-        newTraces.push(getTrace(arrCreatedAt, arrTvoc, t('tvoc')))
-        newTraces.push(getTrace(arrCreatedAt, arrSensorStatus, t('sensorStatus')))
-        newTraces.push(getTrace(arrCreatedAt, arrAutoCalibrationSwitch, t('autoCalibrationSwitch')))
+        const {hcho, voc, temperature, humidity, tvoc, sensorStatus, autoCalibrationSwitch} =
+          cbhchov4
+
+        newTraces.push(getTrace(arrCreatedAt, arrHcho, t('hcho'), hcho))
+        newTraces.push(getTrace(arrCreatedAt, arrVoc, t('voc'), voc))
+        newTraces.push(getTrace(arrCreatedAt, arrTemperature, t('temperature'), temperature))
+        newTraces.push(getTrace(arrCreatedAt, arrHumidity, t('humidity'), humidity))
+        newTraces.push(getTrace(arrCreatedAt, arrTvoc, t('tvoc'), tvoc))
+        newTraces.push(getTrace(arrCreatedAt, arrSensorStatus, t('sensorStatus'), sensorStatus))
+        newTraces.push(
+          getTrace(
+            arrCreatedAt,
+            arrAutoCalibrationSwitch,
+            t('autoCalibrationSwitch'),
+            autoCalibrationSwitch,
+          ),
+        )
 
         setLayout({
           title: t('cbhchov4'),
@@ -351,13 +495,14 @@ const Main: VFC = () => {
           arrTemperature.push(temperature)
           arrHumidity.push(humidity)
         })
+        const {tvoc, pm1p0Grimm, pm2p5Grimm, pm10pGrimm, temperature, humidity} = am1002
 
-        newTraces.push(getTrace(arrCreatedAt, arrTvoc, t('tvoc')))
-        newTraces.push(getTrace(arrCreatedAt, arrPm1p0Grimm, t('pm1P0Grimm')))
-        newTraces.push(getTrace(arrCreatedAt, arrPm2p5Grimm, t('pm2P5Grimm')))
-        newTraces.push(getTrace(arrCreatedAt, arrPm10pGrimm, t('pm10PGrimm')))
-        newTraces.push(getTrace(arrCreatedAt, arrTemperature, t('temperature')))
-        newTraces.push(getTrace(arrCreatedAt, arrHumidity, t('humidity')))
+        newTraces.push(getTrace(arrCreatedAt, arrTvoc, t('tvoc'), tvoc))
+        newTraces.push(getTrace(arrCreatedAt, arrPm1p0Grimm, t('pm1P0Grimm'), pm1p0Grimm))
+        newTraces.push(getTrace(arrCreatedAt, arrPm2p5Grimm, t('pm2P5Grimm'), pm2p5Grimm))
+        newTraces.push(getTrace(arrCreatedAt, arrPm10pGrimm, t('pm10PGrimm'), pm10pGrimm))
+        newTraces.push(getTrace(arrCreatedAt, arrTemperature, t('temperature'), temperature))
+        newTraces.push(getTrace(arrCreatedAt, arrHumidity, t('humidity'), humidity))
 
         setLayout({
           title: t('am1002'),
@@ -370,7 +515,7 @@ const Main: VFC = () => {
     }
 
     return newTraces
-  }, [snChart, modelChart, t, data])
+  }, [snChart, modelChart, t, data, legendsState])
 
   const handleOnData = useCallback<(resGetData: ResGetData) => void>((resGetData) => {
     const {data: d} = resGetData
